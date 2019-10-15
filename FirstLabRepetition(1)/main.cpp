4d4f0b10e415fbc126a5700914827fd6c2b1e64c
Col1nn <pepito98@tlen.pl>
2019-10-15 13:03:52 +0200

    Initial Commit

---
+++ FirstLabRepetition(1)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstLabRepetition(1)
+//
+//  Created by Mateusz Kot on 15/10/2019.
+//  Copyright © 2019 Mateusz Kot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
